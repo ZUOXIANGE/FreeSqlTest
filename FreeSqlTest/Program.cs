d2b2f8757aca82ca77630f6a107fdd4aa26c07e9
zuoxiang <zuoxiang42@gmail.com>
2023-11-28 15:48:29 +0800

    init

---
+++ FreeSqlTest/Program.cs
@@ -0,0 +1,60 @@
+﻿using FreeSqlTest;
+
+Console.WriteLine("开始测试");
+
+var connStr = @"Host=127.0.0.1;Port=8123;Username=root;Password=123;Database=Test";
+
+var fsql = new FreeSql.FreeSqlBuilder()
+    .UseConnectionString(FreeSql.DataType.ClickHouse, connStr)
+    .UseAutoSyncStructure(true)
+    .UseExitAutoDisposePool(false)
+    .Build();
+
+Console.WriteLine("开始创建数据");
+
+var json = "[{\"date\":\"2021-12-19T02:47:53.4365075 08:00\",\"temperatureC\":6,\"temperatureF\":42,\"summary\":\"Balmy\"},{\"date\":\"2021-12-20T02:47:53.4366893 08:00\",\"temperatureC\":36,\"temperatureF\":96,\"summary\":\"Bracing\"},{\"date\":\"2021-12-21T02:47:53.4366903 08:00\",\"temperatureC\":-15,\"temperatureF\":6,\"summary\":\"Bracing\"},{\"date\":\"2021-12-22T02:47:53.4366904 08:00\",\"temperatureC\":14,\"temperatureF\":57,\"summary\":\"Cool\"},{\"date\":\"2021-12-23T02:47:53.4366905 08:00\",\"temperatureC\":29,\"temperatureF\":84,\"summary\":\"Mild\"}]";
+
+var content = "是/否测试Test";
+
+var data = new List<TestTable>();
+for (int i = 0; i < 10; i++)
+{
+    var t = new TestTable
+    {
+        Id = Guid.NewGuid().ToString(),
+        Content = content,
+        Content2 = json,
+        Time = DateTime.Now
+    };
+
+    Console.WriteLine("测试单个插入字符串值改变");
+    await fsql.Insert(t).ExecuteAffrowsAsync();
+
+    data.Add(t);
+}
+
+Console.WriteLine("测试插入超长字符串");
+
+json += json;
+json += json;
+json += json;
+json += json;
+json += json;
+json += json;
+json += json;
+json += json;
+json += json;
+json += json;
+json += json;
+json += json;
+
+data.First().Content2 = json;
+
+//成功插入超长字符串
+await fsql.Insert(data).ExecuteAffrowsAsync();
+
+//单个插入报错
+await fsql.Insert(data.First()).ExecuteAffrowsAsync();
+
+
+Console.WriteLine("插入完成");
---
+++ FreeSqlTest/TestTable.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using FreeSql.DataAnnotations;
+
+namespace FreeSqlTest;
+
+internal class TestTable
+{
+    [Required]
+    [Column(IsIdentity = true)]
+    public string Id { get; set; }
+
+    [Column(StringLength = -2)]
+    public string Content { get; set; }
+
+    [Column(StringLength = -2)]
+    public string Content2 { get; set; }
+
+    [Column(DbType = "DateTime64(3, 'Asia/Shanghai')")]
+    public DateTime Time { get; set; }
+}
